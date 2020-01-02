e0846078784cfbb9708c646d3bb6dc0d077094e2
Devon Grant <dmg4878@rit.edu>
2020-01-02 17:11:22 -0500

    added grid and makes sprite stay in grid (need to be fixed tho check on key press)

--- snakeGame/Snake.py
+++ snakeGame/Snake.py
@@ -1,27 +1,40 @@
 import pyglet
+import random
 
 
 #class to create the snake player
-class Player:
-    def __init__(self, pos_x, pos_y, width, height):
+class Player(object):
+    def __init__(self, pos_x, pos_y, size, velocity):
         self.x = pos_x
         self.y = pos_y
-        self.vel_x = 130
-        self.vel_y = 0
-        self.width = width
-        self.height = height
+        #starts snake in random direction
+        randstart = random.randint(1, 4)
+        if randstart == 1:
+            self.vel_x = velocity
+            self.vel_y = 0
+        elif randstart == 2:
+            self.vel_x = -velocity
+            self.vel_y = 0
+        elif randstart == 3:
+            self.vel_y = velocity
+            self.vel_x = 0
+        elif randstart == 4:
+            self.vel_y = -velocity
+            self.vel_x = 0
+        self.width = size
+        self.height = size
 
         image = pyglet.image.load("images/greenBlock.jpg")
-        image.width = self.width
-        image.height = self.height
+        image.width = size
+        image.height = size
         self.sprite = pyglet.sprite.Sprite(image, x=self.x, y=self.y)
 
     def draw(self):
         self.sprite.draw()
 
     def update(self):
-        self.sprite.x += self.vel_x * (1/60)
-        self.sprite.y += self.vel_y * (1/60)
+        self.sprite.x += self.vel_x * (1/100)
+        self.sprite.y += self.vel_y * (1/100)
 
 
 
--- snakeGame/mainGame.py
+++ snakeGame/mainGame.py
@@ -1,42 +1,93 @@
 import pyglet
+from pyglet.gl import *
+import math
+import tkinter as tk
 from Snake import Player
 from pyglet.window import key
 import random
 
+
 #create the game window
 class GameWindow(pyglet.window.Window):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.set_location(100, 200)
         self.frame_rate = 1/60.0
-
-        self.snake = Player(random.randint(50, 450), random.randint(50, 450), 20, 20)
+        # spawns snake randomly on the screen
+        self.snake = Player(random.choice(listOfPoints)[0], random.choice(listOfPoints)[1], size, velocity)
 
     def on_draw(self):
+        global rows, width
         self.clear()
+        self.drawgrid(width, rows)
         self.snake.sprite.draw()
+
+
 
     def update(self, dt):
         self.snake.update()
 
-#the player movement
-    def on_key_press(self, symbol, modifiers):
-        if symbol == key.RIGHT:
-            self.snake.vel_x = 130
-            self.snake.vel_y = 0
-        elif symbol == key.LEFT:
-            self.snake.vel_x = -130
-            self.snake.vel_y = 0
-        elif symbol == key.UP:
-            self.snake.vel_y = 130
-            self.snake.vel_x = 0
-        elif symbol == key.DOWN:
-            self.snake.vel_y = -130
-            self.snake.vel_x = 0
+# the player movement
+    def on_key_press(self, symbol, modifier):
+        if (symbol == key.RIGHT) or (symbol == key.D):
+            # makes sure snake can only move in the lines (NEEDS TO BE FIXED)
+            # should keep moving in the current direction until it can make the next possible turn
+            tempList = [self.snake.sprite.x, self.snake.sprite.y]
+            if tempList in listOfPoints:
+                self.snake.vel_x = velocity
+                self.snake.vel_y = 0
+        elif (symbol == key.LEFT) or (symbol == key.A):
+            tempList = [self.snake.sprite.x, self.snake.sprite.y]
+            if tempList in listOfPoints:
+                self.snake.vel_x = -velocity
+                self.snake.vel_y = 0
+        elif (symbol == key.UP) or (symbol == key.W):
+            tempList = [self.snake.sprite.x, self.snake.sprite.y]
+            if tempList in listOfPoints:
+                self.snake.vel_y = velocity
+                self.snake.vel_x = 0
+        elif (symbol == key.DOWN) or (symbol == key.S):
+            tempList = [self.snake.sprite.x, self.snake.sprite.y]
+            if tempList in listOfPoints:
+                self.snake.vel_y = -velocity
+                self.snake.vel_x = 0
+        # most recent key
+        mrKey = symbol
+
+# Draws Grid
+    def drawgrid(self, width, rows):
+        global size
+        x = 0
+        y = 0
+        for i in range(rows):
+            x = x + size
+            y = y + size
+            pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', (x, 0, x, width)))
+            pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', (0, y, width, y)))
 
 
 if __name__ == "__main__":
-    window = GameWindow(500, 500, "EPIC SNAKE GAME", resizable=False)
+    # sets rows adn width to global
+    global rows, width, size, velocity, listOfPoints
+    # size of screen
+    width = 500
+    # sets how many rows are in the grid (MUST be divisible by screen width)
+    rows = 20
+    # size of individual squares
+    size = width // rows
+    # speed of slithery boi
+    velocity = 100
+    # list of points (helps keep snake in grid)
+    listOfPoints = []
+    x = 0
+    y = 0
+    for i in range(rows):
+        for j in range(rows):
+            tempList = [x + ((width/rows) * i), y + ((width/rows) * j)]
+            listOfPoints.append(tempList)
+
+    window = GameWindow(width, width, "EPIC SNAKE GAME", resizable=False)
     pyglet.clock.schedule_interval(window.update, window.frame_rate)
     pyglet.app.run()
 
